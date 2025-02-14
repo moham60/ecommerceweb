b9fedbedfe263edcd7219d2e30f01ad782146a66
moham60 <alkingg289@gmail.com>
2025-02-14 17:40:20 +0200

    Updates

---
+++ assets/ProtectedRoute2-DUWmUfub.js
@@ -0,0 +1,1 @@
+import{j as t,q as o}from"./index-F-ZMroND.js";function a({children:e}){return localStorage.getItem("tkn")!==null?t.jsx(o,{to:"/home"}):t.jsx(t.Fragment,{children:e})}export{a as default};
---
+++ assets/useWish-DWvPcvtc.js
@@ -0,0 +1,1 @@
+import{r as s,w as t}from"./index-F-ZMroND.js";function o(){return s.useContext(t)}export{o as u};
