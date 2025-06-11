9df31cd67bf351758daae7897908faec4e6139dc
moham60 <alkingg289@gmail.com>
2025-06-11 23:47:02 +0300

    Updates

---
+++ assets/ProtectedRoute2-Bq5hwTwR.js
@@ -0,0 +1,1 @@
+import{j as t,d as o}from"./index-B3bUcFlg.js";function a({children:e}){return localStorage.getItem("tkn")!==null?t.jsx(o,{to:"/home"}):t.jsx(t.Fragment,{children:e})}export{a as default};
